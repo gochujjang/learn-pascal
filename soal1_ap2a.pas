dd61ff4dc4717410b32bc88e21025b4929c6bafc
gochujjang <41176663+gochujjang@users.noreply.github.com>
2022-05-16 10:29:44 +0700

    Add files via upload

---
+++ soal1_ap2a.pas
@@ -0,0 +1,14 @@
+program soal1_ap2a;
+uses crt;
+var
+    i: integer;
+begin
+    clrscr;
+    i := 0;
+    while i < 5 do
+        begin
+            writeln('Pascal');
+            i := i + 1;
+        end;
+    readln;
+end.
